6a801d4fee0ee1f0a1a9a389ca10419eeefdca7d
trinhthilieu <lieudc23v7x213@dttx.ctu.edu.vn>
2024-11-23 16:31:11 +0700

    Cai dat express chay thong bao xin chao

---
+++ server.js
@@ -0,0 +1,8 @@
+
+const app = require("./app");
+
+const config = require("./app/config");
+const PORT = config.app.port;
+app.listen(PORT, () => {
+  console.log(`Máy chủ đang chạy trên cổng:${PORT}.`);
+});
